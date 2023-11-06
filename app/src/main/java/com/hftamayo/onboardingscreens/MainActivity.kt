96e2dffd76b12c5d16507ec64a2712655a1a96e5
hftamayo <hftamayo@gmail.com>
2023-11-06 10:56:02 -0600

    onboarding design part1

--- app/src/main/java/com/hftamayo/onboardingscreens/MainActivity.kt
+++ app/src/main/java/com/hftamayo/onboardingscreens/MainActivity.kt
@@ -3,17 +3,22 @@ package com.hftamayo.onboardingscreens
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.hftamayo.onboardingscreens.ui.theme.OnBoardingScreensTheme
 
 class MainActivity : ComponentActivity() {
-    val primaryColor = Color(26, 32, 53, 255)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -24,10 +29,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainLayout() {
+    val primaryColor = Color(26, 32, 53, 255)
     NewJetTheme() {
         Surface(
             color = MaterialTheme.colors.background
         ){
+            Column(modifier = Modifier
+                .background(color = primaryColor)
+                .fillMaxSize()
+            ,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                    ,
+                ){
+                    Image(painterResource(id = R.drawable.image),
+                    contentDescription = "",
+                    contentScale = ContentScale.FillWidth,
+                    )
+                }
+                
+            }
 
         }
     }
